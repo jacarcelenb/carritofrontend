89dffd385a992bdd4d009d20a6979d4b969472d5
jacarcelenb <carcelenjorge17@gmail.com>
2022-07-22 22:06:43 -0500

    Commit uno

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
-app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/<app-name>/index.html'));});
+app.use(express.static(__dirname + '/dist/client-view'));
+app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/client-view/index.html'));});
 
 app.listen(process.env.PORT || 8080);
