696fcdf50b386e50e781b236300346b383f0c911
jacarcelenb <carcelenjorge17@gmail.com>
2022-11-14 10:27:31 -0500

    Mejorar el centro del mapa

--- src/app/components/address-app/address-app.component.ts
+++ src/app/components/address-app/address-app.component.ts
@@ -16,28 +16,7 @@ import { AddressService } from 'src/app/service/address.service';
   templateUrl: './address-app.component.html',
   styleUrls: ['./address-app.component.css']
 })
-export class AddressAppComponent implements AfterViewInit, OnInit {
-  @ViewChild('inputPlaces')
-  inputPlaces!: ElementRef;
-  @ViewChild("placesRef") placesRef: GooglePlaceDirective | undefined;
-
-  @ViewChild(GoogleMap) map: GoogleMap | undefined;
-  @ViewChild(MapMarker) markerposition: MapMarker | undefined
-
-  formattedaddress = " ";
-  option = {
-    componentRestrictions: {
-      country: ["AU"]
-    }
-  }
-  markerOptions: google.maps.MarkerOptions = { draggable: true };
-
-  markerPositions: google.maps.LatLngLiteral[] = [];
-  options: google.maps.MapOptions = {
-    center: { lat: -1.831239, lng: -78.183406 },
-    zoom: 4,
-    fullscreenControl: false
-  };
+export class AddressAppComponent implements  OnInit {
 
   list_address: any[] = [];
 
@@ -69,63 +48,6 @@ export class AddressAppComponent implements AfterViewInit, OnInit {
 
     }
     return type_address
-  }
-  public AddressChange(address: any) {
-    //setting address from API to local variable
-    this.formattedaddress = address.formatted_address
-  }
-  addMarker(event: google.maps.MapMouseEvent) {
-    if (this.markerPositions.length == 0) {
-      this.markerPositions.push(event.latLng!.toJSON());
-    }
-  }
-
-  display: boolean = false;
-
-  showDialog() {
-    this.display = true;
-  }
-  public handleAddressChange(address: Address) {
-    console.log(address)
-  }
-
-
-  ngAfterViewInit(): void {
-    const searchBox = new google.maps.places.SearchBox(
-      this.inputPlaces.nativeElement
-    );
-    console.log(searchBox)
-    this.map?.controls[google.maps.ControlPosition.TOP_CENTER].push(
-      this.inputPlaces.nativeElement
-    )
-
-    console.log(this.inputPlaces.nativeElement)
-
-    searchBox.addListener('places_changed', () => {
-      const places = searchBox.getPlaces()
-      if (places?.length == 0) {
-        return;
-      }
-      const bounds = new google.maps.LatLngBounds();
-      places?.forEach(place => {
-        if (!place.geometry || !place.geometry.location) {
-          return;
-        }
-        if (place.geometry.viewport) {
-          bounds.union(place.geometry.viewport)
-        } else {
-          bounds.extend(place.geometry.location)
-        }
-      })
-      this.map?.fitBounds(bounds)
-
-
-    })
-  }
-
-  showValues() {
-    console.log(this.markerposition?.getPosition()?.lat())
-    console.log(this.markerposition?.getPosition()?.lng())
   }
 
 
