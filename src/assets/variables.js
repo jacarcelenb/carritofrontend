a75906b15accf84c888a161f8a3ef660c5026077
jacarcelenb <carcelenjorge17@gmail.com>
2022-12-19 09:56:11 -0500

    Proyecto geolocalización Clientes

--- src/assets/variables.js
+++ src/assets/variables.js
@@ -1,4 +1,4 @@
 export const VARS = Object.freeze({
-  url: 'http://192.168.13.212:8081/',
+  url: 'http://192.168.13.212:8081/oriondir/api/direcciones/',
   headerToken: '123MutImb987.'
 })
--- variables.js
+++
@@ -1,4 +0,0 @@
-export const VARS = Object.freeze({
-  url: 'http://192.168.13.212:8081/oriondir/api/direcciones/',
-  headerToken: '123MutImb987.'
-})
